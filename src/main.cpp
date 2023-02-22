40e79f05c541c565146a2aa26d63fefee159ef4f
JackLeserman <39784006+JackLeserman@users.noreply.github.com>
2023-02-22 17:18:54 -0500

    Works on Laptop motors TBD

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <ros.h>
+#include <ros/time.h>
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Bool.h>
@@ -8,6 +9,7 @@
 #include <Wire.h>
 #include <Adafruit_INA260.h>
 #include <sensor_msgs/BatteryState.h>
+#include "Adafruit_VL53L0X.h"
 
 #include "constants.h"
 
@@ -32,9 +34,46 @@ ros::Publisher pub_bat_level("/battery", &bat_msg);
 ros::Publisher pub_man_override("/manual_override", &man_override);
 ros::Publisher pub_speakers("/play_sound", &speaker_val);
 
+Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
+VL53L0X_RangingMeasurementData_t measure;
+unsigned long range_timer;
 int sound_regulator = 0;
-
 bool override_was_active = false;
+
+void setup_rangefinder(){
+  nh.getHardware()->setBaud(57600);
+  nh.advertise(pub_rf_front);
+  // wait controller to be connected
+  while (!nh.connected()){
+    nh.spinOnce();
+  }
+  // if initialization failed - write message and freeze
+  if (!sensor.begin()) {
+    nh.logwarn("Failed to setup VL53L0X sensor");
+    while(1);
+  }
+  nh.loginfo("VL53L0X API serial node started");
+  // fill static range message fields
+}
+
+void rangefinder(){
+    if ((millis()-range_timer) > 50){
+    sensor.rangingTest(&measure, false);
+    if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+        rf_front_val.data = (float) measure.RangeMilliMeter/1000.0f; // convert mm to m
+        pub_rf_front.publish(&rf_front_val);
+        float p = measure.RangeMilliMeter;
+        char result[20];
+        dtostrf(p, 20, 5, result);
+        nh.logwarn(result);
+    } else {
+      rf_front_val.data = (float) -999999999;
+      pub_rf_front.publish(&rf_front_val);
+      nh.logwarn("Out of range"); // if out of range, don't send message
+    }
+    range_timer =  millis();    
+  }
+}
 
 float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
@@ -63,8 +102,8 @@ void setup() {
 
     esc1.speed(MOTOR_STOP);
     esc2.speed(MOTOR_STOP);
-
     nh.initNode();
+    setup_rangefinder();
     nh.advertise(pub_bat_level);
     nh.subscribe(motor_sub);
     bat_monitor.begin();
@@ -84,8 +123,7 @@ void loop() {
     bat_msg.current = bat_monitor.readCurrent();
     pub_bat_level.publish(&bat_msg);
     //THESE ARE NEEDED TO MAKE MOTOR SPIN ^^^^
-
-    setSpeed(1400);
+    rangefinder();
     override_was_active = true;
     sound_regulator++;
     nh.spinOnce();
