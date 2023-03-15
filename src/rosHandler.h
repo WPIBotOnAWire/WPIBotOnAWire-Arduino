575c297a06ca70a087ff787f15d8e9e8be37f04e
Jwalts23 <81712807+Jwalts23@users.noreply.github.com>
2023-03-15 13:54:19 -0400

    updated bat stuffs

--- src/rosHandler.h
+++ src/rosHandler.h
@@ -2,7 +2,6 @@
 #include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
 #include <std_msgs/Bool.h>
-#include <sensor_msgs/BatteryState.h>
 #include "constants.h"
 class rosHandler{
     public:
@@ -13,7 +12,6 @@ class rosHandler{
         void publishSpeakerVal(int frequency);
         void publishEncoderCounts(int encoderCounts);
         void publishRangeFinders(float rf_front, float rf_back);
-        void publishBatLevels(float current, float voltage);
         void publishFrontRF(float front_val);
         void publishBackRF(float rf_back);
         void logwarn(const char* msg);
@@ -21,15 +19,16 @@ class rosHandler{
         void rfSetup(void);
         bool connected(void);
         void cb_led(const std_msgs::Bool &msg);
+        ros::NodeHandle nh;
         
     private:
 
-        ros::NodeHandle nh;
+        
         
         // messages
         std_msgs::Float32 enc_val, rf_front_val, rf_back_val;
         std_msgs::Bool man_override;
         std_msgs::Int32 speaker_val;
-        sensor_msgs::BatteryState bat_msg;
+
         
 };
