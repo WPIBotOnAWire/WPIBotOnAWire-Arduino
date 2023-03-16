011a1fe5598e479df1eec6addb83c177da3f9694
Jolie Walts <81712807+Jwalts23@users.noreply.github.com>
2023-03-16 12:39:57 -0400

    oop

--- src/main.cpp
+++ src/main.cpp
@@ -18,44 +18,15 @@ Adafruit_INA260 bat_monitor = Adafruit_INA260();
 ESC esc1(ESC1_PIN, MOTOR_FULLBACK, MOTOR_FULLFORWARD, MOTOR_STOP);
 ESC esc2(ESC2_PIN, MOTOR_FULLBACK, MOTOR_FULLFORWARD, MOTOR_STOP);
 
-Encoder encoder(ENCODER_PIN1, ENCODER_PIN2);
-
 ros::NodeHandle nh;
-std_msgs::Float32 enc_val, rf_front_val, rf_back_val;
-std_msgs::Bool man_override;
-std_msgs::Int32 speaker_val;
 sensor_msgs::BatteryState bat_msg;
-ros::Publisher pub_enc("/encoder", &enc_val);
-ros::Publisher pub_rf_front("/rangefinder/front", &rf_front_val);
-ros::Publisher pub_rf_back("/rangefinder/back", &rf_back_val);
 ros::Publisher pub_bat_level("/battery", &bat_msg);
-ros::Publisher pub_man_override("/manual_override", &man_override);
-ros::Publisher pub_speakers("/play_sound", &speaker_val);
 
 int sound_regulator = 0;
 
 bool override_was_active = false;
 
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max) {
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void cb_led(const std_msgs::Bool &msg) {
-    int state = msg.data ? HIGH : LOW;
-    digitalWrite(LED_PIN, state);
-}
-
-void cb_motor(const std_msgs::Float32 &msg) {
-    int speed = mapfloat(msg.data, -1.0, 1.0, MOTOR_FULLBACK, MOTOR_FULLFORWARD);
-    esc1.speed(speed);
-    esc2.speed(speed);
-}
-
-ros::Subscriber<std_msgs::Bool> led_sub("/deterrents/led", &cb_led);
-ros::Subscriber<std_msgs::Float32> motor_sub("/motor_speed", &cb_motor);
-
-void setup() {
-
+void init_motors(){
     esc1.arm();
     esc2.arm();
 
@@ -66,26 +37,30 @@ void setup() {
 
     nh.initNode();
     nh.advertise(pub_bat_level);
-    nh.subscribe(motor_sub);
     bat_monitor.begin();
-    //Serial.begin(9600); // when running robot.launch, comment this out
 }
 
-
 void setSpeed(int throttle){
+    bat_msg.voltage = bat_monitor.readBusVoltage();
+    bat_msg.current = bat_monitor.readCurrent();
+    pub_bat_level.publish(&bat_msg);
+    //THESE ARE NEEDED TO MAKE MOTOR SPIN ^^^^
+    
     // Sets the speed of the motors with a given input
     esc1.speed(throttle);
     esc2.speed(throttle);
 }
 
+void setup() {
+
+    init_motors();
+    //Serial.begin(9600); // when running robot.launch, comment this out
+}
+
 
 void loop() {
-    bat_msg.voltage = bat_monitor.readBusVoltage();
-    bat_msg.current = bat_monitor.readCurrent();
-    pub_bat_level.publish(&bat_msg);
-    //THESE ARE NEEDED TO MAKE MOTOR SPIN ^^^^
-
     setSpeed(1400);
+
     override_was_active = true;
     sound_regulator++;
     nh.spinOnce();
