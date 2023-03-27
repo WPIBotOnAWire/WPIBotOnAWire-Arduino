9b6abb45f7864119ff9d86a0d6229d9a3874c7d6
JackLeserman <39784006+JackLeserman@users.noreply.github.com>
2023-03-27 13:11:31 -0400

    encoder tets

--- src/main.cpp
+++ src/main.cpp
@@ -27,9 +27,9 @@ ros::NodeHandle nh;
 sensor_msgs::BatteryState bat_msg;
 ros::Publisher pub_bat_level("/battery", &bat_msg);
 
-std_msgs::Float32 enc_val, rf_front_val, rf_back_val;
+std_msgs::Float32 rf_front_val, rf_back_val;
 std_msgs::Bool man_override;
-std_msgs::Int32 speaker_val;
+std_msgs::Int32 speaker_val, enc_val;
 ros::Publisher pub_enc("/encoder", &enc_val);
 ros::Publisher pub_rf_front("/rangefinder/front", &rf_front_val);
 ros::Publisher pub_rf_back("/rangefinder/back", &rf_back_val);
@@ -165,8 +165,9 @@ void drive_forward_meters(long meters){
 }
 
 void publishEncCounts(){
-    int ecounts = EC.get_encoder_counts();
-    enc_val.data = ecounts;
+    //int ecounts = EC.get_encoder_counts();
+    enc_val.data = 1000;
+    nh.loginfo("Enc val" + (int)enc_val.data);
     pub_enc.publish(&enc_val);
 }
 
@@ -182,7 +183,7 @@ void setup() {
 void loop() {
     updateBat();
     rangefinder();
-    // publishEncCounts();
+    publishEncCounts();
     override_was_active = true;
     sound_regulator++;
     nh.spinOnce();
