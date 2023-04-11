3846e77deabef342a81c07cf408c8415194d8f16
JackLeserman <39784006+JackLeserman@users.noreply.github.com>
2023-04-11 17:07:26 -0400

    Revert "Working line pubsub"
    This reverts commit 45ed1425640d1507f8bcc6345f6eb5c50274b090.

--- src/main.cpp
+++ src/main.cpp
@@ -12,135 +12,221 @@
 #include "constants.h"
 #include <RotaryEncoder.h>
 #define USE_USBCON
-#define LOX1 0x30
-#define LOX2 0x31
-#define LOX3 0x32
 
-// Shutdown pins for VL53L0X (XSHUT)
-#define XS_LOX1 30
-#define XS_LOX2 31
-#define XS_LOX3 32
+// battery Monitor
+Adafruit_INA260 bat_monitor = Adafruit_INA260();
 
-// Objects for the ToF sensor
-Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
-Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
-Adafruit_VL53L0X lox3 = Adafruit_VL53L0X();
+ESC esc1(ESC1_PIN, MOTOR_FULLBACK, MOTOR_FULLFORWARD, MOTOR_STOP);
+ESC esc2(ESC2_PIN, MOTOR_FULLBACK, MOTOR_FULLFORWARD, MOTOR_STOP);
 
-// Measurement variables
-VL53L0X_RangingMeasurementData_t measure1;
-VL53L0X_RangingMeasurementData_t measure2;
-VL53L0X_RangingMeasurementData_t measure3;
 
-double sensor1, sensor2, sensor3;
+// Setup a RotaryEncoder with 4 steps per latch for the 2 signal input pins:
+// RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::FOUR3);
+// Setup a RotaryEncoder with 2 steps per latch for the 2 signal input pins:
+RotaryEncoder encoder(PIN_IN1, PIN_IN2, RotaryEncoder::LatchMode::TWO03);
+int encoder_counts=0;
 
-// Process to assign new I2C addresses
-void setID() {
-  // RESET procedure
-  digitalWrite(XS_LOX1, LOW);
-  digitalWrite(XS_LOX2, LOW);
-  digitalWrite(XS_LOX3, LOW);
-  delay(10);
-  digitalWrite(XS_LOX1, HIGH);
-  digitalWrite(XS_LOX2, HIGH);
-  digitalWrite(XS_LOX3, HIGH);
+ros::NodeHandle nh;
+sensor_msgs::BatteryState bat_msg;
+ros::Publisher pub_bat_level("/battery", &bat_msg);
 
-  // Activate L0X1 and reset other sensors
-  digitalWrite(XS_LOX1, HIGH);
-  digitalWrite(XS_LOX2, LOW);
-  digitalWrite(XS_LOX3, LOW);
+std_msgs::Float32 rf_front_val, rf_back_val;
+std_msgs::Bool man_override;
+std_msgs::Int32 speaker_val, enc_val;
+ros::Publisher pub_enc("/encoder", &enc_val);
+ros::Publisher pub_rf_front("/rangefinder/front", &rf_front_val);
+ros::Publisher pub_rf_back("/rangefinder/back", &rf_back_val);
+ros::Publisher pub_man_override("/manual_override", &man_override);
+ros::Publisher pub_speakers("/play_sound", &speaker_val);
 
-  // Initialize LOX1
-  if (!lox1.begin(LOX1)) {
-    Serial.println(F("Failed to boot LOX1 VL53L0X"));
-    while (1);
+Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
+VL53L0X_RangingMeasurementData_t measure;
+unsigned long range_timer;
+int sound_regulator = 0;
+bool override_was_active = false;
+
+void stopMotors(){
+    esc1.speed(MOTOR_STOP);
+    esc2.speed(MOTOR_STOP);
+}
+
+void updateBat(){
+    //THESE GET THE BATTERY INFO AND ARE NEEDED TO MAKE MOTOR SPIN ^^^^
+    bat_msg.voltage = bat_monitor.readBusVoltage();
+    bat_msg.current = bat_monitor.readCurrent();
+    pub_bat_level.publish(&bat_msg);
+}
+
+
+void setThrottle(int throttle){
+    updateBat();
+
+    // Sets the speed of the motors with a given input
+    esc1.speed(throttle);
+    esc2.speed(throttle);
+    // Serial.println("Driving at ");
+    // Serial.print(throttle);
+    // Serial.println("");
+}
+
+void setSpeed(int percent){
+  int value = percent*1 + 1500;
+  if (percent == 0){
+    stopMotors();
+  } else{
+    setThrottle(value);
   }
+  
 
-  // Initialize LOX2
-  digitalWrite(XS_LOX2, HIGH);
-  delay(10);
+}
 
-  if (!lox2.begin(LOX2)) {
-    Serial.println(F("Failed to boot LOX2 VL53L0X"));
-    while (1);
+
+// function to control leds in state machine
+void cb_led(const std_msgs::Bool &msg) {
+    int state = msg.data ? HIGH : LOW;
+    digitalWrite(LED_PIN, state);
+}
+
+// function to control motors in state machine
+void cb_motor(const std_msgs::Float32 &msg) {
+    //int speed = mapfloat(msg.data, -1.0, 1.0, MOTOR_FULLBACK, MOTOR_FULLFORWARD);
+    int speed = msg.data;
+    setSpeed(speed);
+}
+
+ros::Subscriber<std_msgs::Bool> led_sub("/deterrents/led", &cb_led);
+ros::Subscriber<std_msgs::Float32> motor_sub("/motor_speed", &cb_motor);
+
+void init_motors(){
+    //sets up the ESCs and battery info to allow them to spin
+    //ESCs need battery to spin. 2021-22 team was a bunch of drone bros and picked this garbage
+    esc1.arm();
+    esc2.arm();
+
+    delay(500);
+
+    stopMotors();
+
+    nh.initNode();
+    nh.advertise(pub_bat_level);
+    nh.advertise(pub_enc);
+    nh.subscribe(motor_sub);
+    bat_monitor.begin();
+
+}
+
+void setup_rangefinder(){
+  nh.getHardware()->setBaud(9600);
+  nh.advertise(pub_rf_front);
+  
+  // wait controller to be connected
+  while (!nh.connected()){
+    nh.spinOnce();
   }
-  // Initialize LOX3
-  digitalWrite(XS_LOX3, HIGH);
-  delay(10);
+  // if initialization failed - write message and freeze
+  if (!sensor.begin()) {
+    // nh.logwarn("Failed to setup VL53L0X sensor");
+    while(1);
+  }
+  nh.loginfo("VL53L0X API serial node started");
+  // fill static range message fields
+  
+}
 
-  if (!lox3.begin(LOX3)) {
-    Serial.println(F("Failed to boot LOX3 VL53L0X"));
-    while (1);
+void rangefinder(){
+    if ((millis()-range_timer) > 50){
+    sensor.rangingTest(&measure, false);
+    if (measure.RangeStatus != 4) {  // phase failures have incorrect data
+        rf_front_val.data = (float) measure.RangeMilliMeter/1000.0f; // convert mm to m
+        pub_rf_front.publish(&rf_front_val);
+        float p = measure.RangeMilliMeter /1000.0f; // m
+        char result[20];
+        dtostrf(p, 20, 5, result);
+        // nh.logwarn(result);
+    } else {
+      rf_front_val.data = (float) -999999999;
+      pub_rf_front.publish(&rf_front_val);
+      // nh.logwarn("Out of range"); // if out of range, don't send message
+    }
+    range_timer =  millis();    
   }
 }
 
-void readLOX() {
-  lox1.rangingTest(&measure1, false); // pass in 'true' to get data print
-  lox2.rangingTest(&measure2, false);
-  lox3.rangingTest(&measure3, false);
-
-  // Print reading from LOX1
-  Serial.print("LOX1: ");
-  if (measure1.RangeStatus != 4) {   // if not out of range
-    sensor1 = measure1.RangeMilliMeter;
-    Serial.print(sensor1);
-    Serial.print("mm");
-  } else {
-    Serial.print("Out of range");
-  }
-
-  Serial.print(" ");
-
-  // Print reading from LOX2
-  Serial.print("LOX2: ");
-  if (measure2.RangeStatus != 4) {   // if not out of range
-    sensor2 = measure2.RangeMilliMeter;
-    Serial.print(sensor2);
-    Serial.print("mm");
-  } else {
-    Serial.print("Out of range");
-  }
-  Serial.print(" ");
-
-  Serial.print("LOX3: ");
-  if (measure3.RangeStatus != 4) {   // if not out of range
-    sensor3 = measure3.RangeMilliMeter;
-    Serial.print(sensor3);
-    Serial.print("mm");
-  } else {
-    Serial.print("Out of range");
-  }
-  Serial.println();
+void drive_rpm(double target_speed){
+    // float pid_speed = EC.pid_effort_rpm(target_speed);
+    // // setSpeed(pid_speed);
+    // Serial.println("Driving at PID ");
+    // Serial.print(pid_speed);
+    // Serial.println("");
 }
 
-void sensorConfig() {
-  lox1.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT);
-  lox2.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT);
-  lox3.configSensor(Adafruit_VL53L0X::VL53L0X_SENSE_DEFAULT);
-
-  /* Set desired configurations from the options below:
-   *    VL53L0X_SENSE_DEFAULT
-        VL53L0X_SENSE_LONG_RANGE
-        VL53L0X_SENSE_HIGH_SPEED
-        VL53L0X_SENSE_HIGH_ACCURACY
-   */
+void drive_forward_meters(long meters){
+    // if(dist_traveled >= inches){
+    //     setSpeed(1500);
+    // }else{
+    //     setSpeed(1550);
+    // }
 }
+//  encoder stuff
+void setup_encoder(){
+  // while (! Serial);
+  // Serial.println("LimitedRotator example for the RotaryEncoder library.");
+  encoder.setPosition(10 / ROTARYSTEPS); // start with the value of 10.
+}
+
+void publishEncCounts(int ecounts){
+    enc_val.data = ecounts;
+    // Serial.print(enc_val.data);
+    // Serial.println();
+    pub_enc.publish(&enc_val);
+    nh.spinOnce();
+}
+
+int lastPos = -1;
+int encodercount = 0;
+void encoderCounts(){
+    encoder.tick();
+  // get the current physical position and calc the logical position
+  int newPos = encoder.getPosition() * ROTARYSTEPS;
+  if (newPos < ROTARYMIN) {
+    encoder.setPosition(ROTARYMIN / ROTARYSTEPS);
+    newPos = ROTARYMIN;
+  } else if (newPos > ROTARYMAX) {
+    encoder.setPosition(ROTARYMAX / ROTARYSTEPS);
+    newPos = ROTARYMAX;
+  } // if
+  if (lastPos != newPos) {
+    lastPos = newPos;
+    encodercount = newPos;
+    
+  } // if
+  publishEncCounts(encodercount);
+}
+
+
+/*
+void initAllNodes(){
+  
+}
+*/
 
 void setup() {
-  Serial.begin(115200);
-  // Wait until serial port opens for native USB devices
-  while (!Serial) {
-    delay(1);
-  }
-  pinMode(XS_LOX1, OUTPUT);
-  pinMode(XS_LOX2, OUTPUT);
-  pinMode(XS_LOX3, OUTPUT);
-
-  Serial.println("Starting...");
-  setID();
-  sensorConfig();
+    init_motors();
+    // setup_rangefinder();
+    setup_encoder();
+    // pinMode(RADIO_OVERRIDE_PIN, INPUT);
+    // Serial.begin(9600); // when running robot.launch, comment this out
 }
-
+  
+long timer;
 void loop() {
-  readLOX();
-  delay(100);
-}
+    if(millis()-timer> 1000){
+      // updateBat();
+      // rangefinder();
+      // encoderCounts();
+      override_was_active = true;
+      sound_regulator++;
+      timer = millis();
+    }
+  nh.spinOnce();
+} 
