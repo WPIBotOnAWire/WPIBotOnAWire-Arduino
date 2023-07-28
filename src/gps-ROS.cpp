af8c8fddd4321bed361fbc5776a4e257ef6a2f1a
Greg <lewin.gc@gmail.com>
2023-07-28 13:11:22 -0400

    working sending NMEA strings; not parsing

--- src/gps-ROS.cpp
+++ src/gps-ROS.cpp
@@ -5,8 +5,9 @@
 
 #include <gps.h>
 
-#include <std_msgs/Int32.h>
-
+#include <gps_common/GPSFix.h>
+#include <std_msgs/String.h>
+//using namespace gps_common;
 
 /*
  * setup serial for the GPS on SERCOM2: 
@@ -22,8 +23,18 @@ void SERCOM2_Handler()
   gpsSerial.IrqHandler();
 }
 
-std_msgs::Int32 gps_report;
-ros::Publisher pub_gps("/GPS", &gps_report);
+/**
+ * Need to figure out the whole gps_common thing...might need to custom build the messages???
+ * 
+ * In the meantime, I'll just pass a string and move on.
+*/
+gps_common::GPSFix gpsFix;
+gps_common::GPSStatus gpsStatus;
+// ros::Publisher pub_gps_fix("GPSfix", &gpsFix);
+// ros::Publisher pub_gps_status("GPSStatus", &gpsStatus);
+
+std_msgs::String gpsNMEA;
+ros::Publisher pubNMEA("strNMEA", &gpsNMEA);
 
 void setupGPS(ros::NodeHandle& nh) 
 {
@@ -46,18 +57,96 @@ void setupGPS(ros::NodeHandle& nh)
   //gps.SetReportTime(2000);
   //gps.SendNMEA("PMTK314,0,5,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0");
 
-  nh.advertise(pub_gps);
+//   nh.advertise(pub_gps_fix);
+//   nh.advertise(pub_gps_status);
+    nh.advertise(pubNMEA);
 
   SerialUSB.println(F("/setupGPS()"));
 }
 
+/**
+ * Parses an NMEA string and puts data in ROS messages
+*/
+GPSDatum ParseNMEA_ROS(const String& nmeaStr)
+{
+    //SerialUSB.println(nmeaStr);
+    GPSDatum gpsDatum;
+    
+    uint16_t length = nmeaStr.length();
+    if(length < 3) return 0;
+    
+    if(nmeaStr[0] != '$') return 0;
+    if(nmeaStr[length - 3] != '*') return 0;
+    
+    uint8_t checksum = GPS::CalcChecksum(nmeaStr.substring(1, length - 3));
+    String checksumStr = String(checksum, HEX);
+    checksumStr.toUpperCase();
+    String checksumNMEA = nmeaStr.substring(length - 2);
+    
+    if(checksumStr != checksumNMEA) return gpsDatum;
+    
+    String NMEAtype = GPSDatum::GetNMEASubstring(nmeaStr, 0);
+    
+    if (NMEAtype == "GPGGA")
+    {
+        gpsDatum.gpsFix = GPSDatum::GetNMEASubstring(nmeaStr, 6).toInt();
+        //if(!gpsDatum.gpsFix) return gpsDatum;
+        
+        // time
+        gpsDatum.NMEAtoTime(GPSDatum::GetNMEASubstring(nmeaStr, 1));
+        
+        gpsDatum.lat = GPSDatum::ConvertToDMM(GPSDatum::GetNMEASubstring(nmeaStr, 2));
+        if(GPSDatum::GetNMEASubstring(nmeaStr, 3) == String('S')) gpsDatum.lat *= -1;
+        
+        gpsDatum.lon = GPSDatum::ConvertToDMM(GPSDatum::GetNMEASubstring(nmeaStr, 4));
+        if(GPSDatum::GetNMEASubstring(nmeaStr, 5) == String('W')) gpsDatum.lon *= -1;
+        
+        gpsDatum.elevDM = GPSDatum::GetNMEASubstring(nmeaStr, 9).toFloat() * 10;
+        
+        gpsDatum.source = GGA;
+
+        gpsFix.status.position_source = gpsDatum.gpsFix;
+        gpsFix.latitude = GPSDatum::GetNMEASubstring(nmeaStr, 2).toFloat();
+        gpsFix.longitude = GPSDatum::GetNMEASubstring(nmeaStr, 4).toFloat();
+
+        gpsNMEA.data = nmeaStr.c_str();
+        pubNMEA.publish(&gpsNMEA);
+
+        //pub_gps_fix.publish(&gpsFix);
+
+        return gpsDatum;
+    }
+    
+    if(NMEAtype == "GPRMC")
+    {
+        if(GPSDatum::GetNMEASubstring(nmeaStr, 2) != String('A')) return gpsDatum;
+        //else gpsDatum.gpsFix = 1;
+        
+        gpsDatum.lat = GPSDatum::ConvertToDMM(GPSDatum::GetNMEASubstring(nmeaStr, 3));
+        if(GPSDatum::GetNMEASubstring(nmeaStr, 4) == String('S')) gpsDatum.lat *= -1;
+        
+        gpsDatum.lon = GPSDatum::ConvertToDMM(GPSDatum::GetNMEASubstring(nmeaStr, 5));
+        if(GPSDatum::GetNMEASubstring(nmeaStr, 6) == String('W')) gpsDatum.lon *= -1;
+        
+        //gpsDatum.speed = GetNMEASubstring(nmeaStr, 7).toFloat();
+        
+        gpsDatum.NMEAtoTime(GPSDatum::GetNMEASubstring(nmeaStr, 1));
+        gpsDatum.NMEAtoDate(GPSDatum::GetNMEASubstring(nmeaStr, 9));
+        
+        gpsDatum.source = RMC;
+        return gpsDatum;
+    }
+    
+    return 0;
+}
+
 void processGPS(void) 
 {
-  if(gps.CheckSerial()) // & GGA) //if we have a GGA
-  {
-    // this spends time trying to consolidate readings...might want to undo and use ROS constructs
-    String reportStr = gps.GetReading().MakeDataString();
-
-    SerialUSB.println(reportStr);  
-  }
+    String nmeaStr;
+    if(gps.CheckSerialRaw(nmeaStr)) // & GGA) //if we have a GGA
+    {
+        // SerialUSB.println(nmeaStr); 
+        ParseNMEA_ROS(nmeaStr); 
+    }
 }
+
