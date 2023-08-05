623ad3994c51b027ad03077268751b0b816f10d0
Greg <lewin.gc@gmail.com>
2023-08-05 10:04:47 -0400

    now with factors; untested

--- src/position_filter.cpp
+++ src/position_filter.cpp
@@ -1,21 +1,26 @@
 #include "position_filter.h"
 
+const float RAD_PER_DMM = M_PI / (180 * 60 * 10000); //precision problem?
+
 /**
  * Setup the geometry.
 */
 Location::Location(int32_t lat1, int32_t lon1, int32_t lat2, int32_t lon2, float a)
 {
+    vertexH = a;
+
     LAT1DMM = lat1;
     LON1DMM = lon1;
     
     LAT2DMM = lat2;
     LON2DMM = lon2;
 
+    //centerpoint; ignores great circle for such a short distance
     LAT0DMM = (LAT1DMM + LAT2DMM) / 2;
     LON0DMM = (LON1DMM + LON2DMM) / 2;
 
-    metersPerDMMLat = 0; //?????
-    metersPerDMMLon = metersPerDMMLat * cos(LAT0DMM / 0); //needs factor!!!
+    metersPerDMMLat = 0.185185185; // 10,000 km over 90 degrees
+    metersPerDMMLon = metersPerDMMLat * cos(LAT0DMM * RAD_PER_DMM);
 
     // find the delta
     float deltaXi = (LON2DMM - LON0DMM) * metersPerDMMLon;
@@ -42,7 +47,7 @@ float Location::updateFromGPS(int32_t latDMM, int32_t lonDMM)
     // dot the GPS vector with the unit vector of the cable to get x 
     // (ie, project the GPS reading to conform to the cable)
     float currX = distLat * unitXi + distLon * unitEta;
-    float currS = vertexH * sinh(currX / vertexH);
+    float currS = vertexH * sinh(currX / vertexH); // sinh is expensive, but the only one
 
     // do I want/need to update x,s datums?
 
