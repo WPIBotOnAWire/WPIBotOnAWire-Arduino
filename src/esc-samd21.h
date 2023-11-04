2e08af78267564e83c5f69b18503bd7074880a94
Greg Lewin <gcl8a@icloud.com>
2023-11-04 17:18:57 -0400

    added event timer

--- src/esc-samd21.h
+++ src/esc-samd21.h
@@ -1,10 +1,14 @@
 #pragma once
 
 #include <Arduino.h>
+#include <event_timer.h>
 
 class ESCDirect
 {
 private:
+    enum MOTOR_STATE {IDLE, AMRING, CAL_LOW, CAL_HIGH, STOPPED, ACTIVE};
+    MOTOR_STATE motorState = IDLE;
+
     // Calibration
     int oMin = 1000; 
     int oMid = 1500;
@@ -12,9 +16,10 @@ private:
     int oMax = 2000;
     int oArm = 500;
 
-    uint32_t calibrationDelay = 8000;	// Calibration delay (millisecond)
+    uint32_t calibrationDelay = 3000;	// Calibration delay (millisecond)
 
     void WriteMicroseconds(uint16_t uSec);
+    void WriteMin(uint32_t duration) {}
 
 public:
     void Init(void);
