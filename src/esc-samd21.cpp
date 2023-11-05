342de4c5a92ab6139f89f5722e788e8a256cffd8
Greg Lewin <gcl8a@icloud.com>
2023-11-05 08:13:43 -0500

    deleted vestigial commented out stuff; updated desc

--- src/esc-samd21.cpp
+++ src/esc-samd21.cpp
@@ -3,11 +3,11 @@
 #include <Arduino.h>
 
 /**
- * This sets up TCC0 to send "RC-servo" pulses to pins 10 and 13. We skip the Arduino Servo library
+ * This sets up TCC0 to send "RC-servo" pulses to pin 2. We skip the Arduino Servo library
  * (which is a hack) and use the timers directly. This will produce a much smoother pulse output.
  * 
- * Note that we set up two channels, one for each motor, which is somewhat redundant, as both
- * motors are sent the same command. Perhaps there will someday be a need to control independently?
+ * Note that we only set up one channel, as both motors are sent the same command. 
+ * Perhaps there will someday be a need to control independently?
 */
 void ESCDirect::Init(void)
 {
@@ -20,20 +20,6 @@ void ESCDirect::Init(void)
                      GCLK_GENCTRL_SRC_DFLL48M |   // Set the 48MHz clock source
                      GCLK_GENCTRL_ID(4);          // Select GCLK4
   while (GCLK->STATUS.bit.SYNCBUSY);              // Wait for synchronization
-
-//   // Enable the port multiplexer for digital pin 13 (D13; PA17): timer TCC0 output
-//   PORT->Group[g_APinDescription[13].ulPort].PINCFG[g_APinDescription[13].ulPin].bit.PMUXEN = 1;
-  
-//   // Connect the TCC0 timer to the port output - port pins are paired odd PMUXO and even PMUXE
-//   // F & E specify the timers: TCC0, TCC1 and TCC2
-//   PORT->Group[g_APinDescription[13].ulPort].PMUX[g_APinDescription[13].ulPin >> 1].reg = PORT_PMUX_PMUXO_F;
-  
-//   // Enable the port multiplexer for digital pin 10 (D10; PA18): timer TCC0 output
-//   PORT->Group[g_APinDescription[10].ulPort].PINCFG[g_APinDescription[10].ulPin].bit.PMUXEN = 1;
-  
-//   // Connect the TCC0 timer to the port output - port pins are paired odd PMUXO and even PMUXE
-//   // F & E specify the timers: TCC0, TCC1 and TCC2
-//   PORT->Group[g_APinDescription[10].ulPort].PMUX[g_APinDescription[10].ulPin >> 1].reg = PORT_PMUX_PMUXE_F;
   
   // Enable the port multiplexer for digital pin 2 (D2; PA14): timer TCC0 output
   PORT->Group[g_APinDescription[2].ulPort].PINCFG[g_APinDescription[2].ulPin].bit.PMUXEN = 1;
@@ -41,7 +27,6 @@ void ESCDirect::Init(void)
   // Connect the TCC0 timer to the port output - port pins are paired odd PMUXO and even PMUXE
   // F & E specify the timers: TCC0, TCC1 and TCC2
   PORT->Group[g_APinDescription[2].ulPort].PMUX[g_APinDescription[2].ulPin >> 1].reg = PORT_PMUX_PMUXE_F;
-
 
   // Feed GCLK4 to TCC0 and TCC1
   REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TCC0 and TCC1
@@ -59,14 +44,6 @@ void ESCDirect::Init(void)
   // 20000 = 50Hz, 10000 = 100Hz, 2500  = 400Hz
   REG_TCC0_PER = 20000;      // Set the frequency of the PWM on TCC0 to 50Hz
   while(TCC0->SYNCBUSY.bit.PER);
-
-//   // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-//   REG_TCC0_CCB3 = oMid;       // TCC0 CCB3 - center the servo on D13
-//   while(TCC0->SYNCBUSY.bit.CCB3);
-
-//   // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-//   REG_TCC0_CCB2 = oMid;       // TCC0 CCB2 - center the servo on D10
-//   while(TCC0->SYNCBUSY.bit.CCB2);
 
   // The CCBx register value corresponds to the pulsewidth in microseconds (us)
   REG_TCC0_CCB0 = oMid;       // TCC0 CCB0 - center the servo on D2
@@ -101,14 +78,6 @@ ESCDirect::MOTOR_STATE ESCDirect::Arm(void)
 
 void ESCDirect::WriteMicroseconds(uint16_t uSec)
 {
-    // // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-    // REG_TCC0_CCB3 = uSec;       // TCC0 CCB3 - center the servo on D13
-    // while(TCC0->SYNCBUSY.bit.CCB3);
-
-    // // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-    // REG_TCC0_CCB2 = uSec;       // TCC0 CCB0 - center the servo on D12
-    // while(TCC0->SYNCBUSY.bit.CCB2);
-
     // The CCBx register value corresponds to the pulsewidth in microseconds (us)
     REG_TCC0_CCB0 = uSec;       // TCC0 CCB0 - center the servo on D12
     while(TCC0->SYNCBUSY.bit.CCB0);
