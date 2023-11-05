43dd1229294797b1d27a3e09c697b268eb8f0abe
Greg Lewin <gcl8a@icloud.com>
2023-11-04 20:45:31 -0400

    added pin 2 for ESC -- need to test

--- src/esc-samd21.cpp
+++ src/esc-samd21.cpp
@@ -2,12 +2,6 @@
 
 #include <Arduino.h>
 
-// Motor Constants -- TODO: pass to constructor
-// fullforward = 2000, fullback = 1000
-#define MOTOR_FULLFORWARD    2000
-#define MOTOR_FULLBACK       1000
-#define MOTOR_STOP           1500
-
 /**
  * This sets up TCC0 to send "RC-servo" pulses to pins 10 and 13. We skip the Arduino Servo library
  * (which is a hack) and use the timers directly. This will produce a much smoother pulse output.
@@ -41,6 +35,14 @@ void ESCDirect::Init(void)
   // F & E specify the timers: TCC0, TCC1 and TCC2
   PORT->Group[g_APinDescription[10].ulPort].PMUX[g_APinDescription[10].ulPin >> 1].reg = PORT_PMUX_PMUXE_F;
   
+  // Enable the port multiplexer for digital pin 2 (D2; PA14): timer TCC0 output
+  PORT->Group[g_APinDescription[2].ulPort].PINCFG[g_APinDescription[2].ulPin].bit.PMUXEN = 1;
+  
+  // Connect the TCC0 timer to the port output - port pins are paired odd PMUXO and even PMUXE
+  // F & E specify the timers: TCC0, TCC1 and TCC2
+  PORT->Group[g_APinDescription[2].ulPort].PMUX[g_APinDescription[2].ulPin >> 1].reg = PORT_PMUX_PMUXE_F;
+
+
   // Feed GCLK4 to TCC0 and TCC1
   REG_GCLK_CLKCTRL = GCLK_CLKCTRL_CLKEN |         // Enable GCLK4 to TCC0 and TCC1
                      GCLK_CLKCTRL_GEN_GCLK4 |     // Select GCLK4
@@ -59,11 +61,11 @@ void ESCDirect::Init(void)
   while(TCC0->SYNCBUSY.bit.PER);
 
   // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-  REG_TCC0_CCB3 = oArm;       // TCC0 CCB3 - center the servo on D13
+  REG_TCC0_CCB3 = oMid;       // TCC0 CCB3 - center the servo on D13
   while(TCC0->SYNCBUSY.bit.CCB3);
 
   // The CCBx register value corresponds to the pulsewidth in microseconds (us)
-  REG_TCC0_CCB2 = oArm;       // TCC0 CCB0 - center the servo on D12
+  REG_TCC0_CCB2 = oMid;       // TCC0 CCB0 - center the servo on D12
   while(TCC0->SYNCBUSY.bit.CCB2);
 
   // Divide the 16MHz signal by 8 giving 2MHz (0.5us) TCC0 timer tick and enable the outputs
@@ -73,13 +75,24 @@ void ESCDirect::Init(void)
 }
 
 /*
- * Sent a signal to Arm the ESC
- * depends on the Arming value from the constructor
+ * We don't actually do anything here. The ESC arms by default when the
+ * uC powers up (see the Init() function). So we'll just check that 3 
+ * seconds has elapsed before we allow commands.
  */
-void ESCDirect::Arm(void)
+ESCDirect::MOTOR_STATE ESCDirect::Arm(void)
 {
-	WriteMicroseconds(oArm);
-    delay(500); //might need to make non-blocking
+    switch(motorState)
+    {
+        case IDLE:
+            if(millis() > 5000)
+            motorState = ARMED;
+            break;
+
+        default:
+            break;
+    }
+
+    return motorState;
 }
 
 void ESCDirect::WriteMicroseconds(uint16_t uSec)
@@ -97,28 +110,24 @@ void ESCDirect::WriteMicroseconds(uint16_t uSec)
  * Sent a signal to set the ESC speed
  * depends on the calibration minimum and maximum values
  */
-void ESCDirect::SetSpeed(int16_t pct)
+ESCDirect::MOTOR_STATE ESCDirect::SetSpeed(int16_t pct)
 {
-    if(pct == 0)
+    if(motorState != ARMED) return motorState;
+    
+    else
     {
-        Stop();
-        return;
+        if(pct == 0)
+        {
+            Stop();
+        }
+
+        else
+        {
+            uint16_t speed = oMid + pct * (oMax - oMin) / 200;
+            uint16_t pulseUS = constrain(speed, oMin, oMax);
+            WriteMicroseconds(pulseUS);
+        }
     }
 
-    uint16_t speed = oMid + pct * (oMax - oMin) / 200;
-	uint16_t pulseUS = constrain(speed, oMin, oMax);
-	WriteMicroseconds(pulseUS);
-}
-
-/*
- * Calibrate the maximum and minimum PWM signal the ESC is expecting
- * depends on the outputMin, outputMax values from the constructor
- */
-void ESCDirect::Calibrate(void)
-{
-	WriteMicroseconds(oMax);
-    delay(calibrationDelay);
-    WriteMicroseconds(oMin);
-	delay(calibrationDelay);
-	Arm();
+    return motorState;
 }
--- src/esc-samd21.h
+++ src/esc-samd21.h
@@ -3,28 +3,48 @@
 #include <Arduino.h>
 #include <event_timer.h>
 
+// Motor Constants -- TODO: pass to constructor
+// fullforward = 2000, fullback = 1000
+#define MOTOR_FULL_REV    1000
+#define MOTOR_STOP        1500
+#define MOTOR_FULL_FWD    2000
+
+/**
+ * There is some confusion about how the ESCs are set up. The datasheet has
+ * lots of options for calibrating, but it appears they have a fixed calibration range (1000 - 2000us).
+ * 
+ * Experiments show that you only have to set the signal to 1500us for a 
+ * 'few moments' and it beep-a-leeps at you.
+ * 
+ * So, we'll remove the Calibration function and just default to 1500us
+ * in the Init() function. Then we'll need a timestamp for checking if it's
+ * armed and call it good.
+ */
+
 class ESCDirect
 {
+public:
+    enum MOTOR_STATE {IDLE, ARMED};
+
 private:
-    enum MOTOR_STATE {IDLE, AMRING, CAL_LOW, CAL_HIGH, STOPPED, ACTIVE};
     MOTOR_STATE motorState = IDLE;
+    EventTimer motorTimer;
 
     // Calibration
-    int oMin = 1000; 
-    int oMid = 1500;
-    int oStop = 1500;
-    int oMax = 2000;
-    int oArm = 500;
+    int oMin = MOTOR_FULL_REV; 
+    int oMid = MOTOR_STOP;
+    int oStop = MOTOR_STOP;
+    int oMax = MOTOR_FULL_FWD;
 
-    uint32_t calibrationDelay = 3000;	// Calibration delay (millisecond)
+    uint32_t armingDelay = 3000;	// Calibration delay (millisecond)
 
     void WriteMicroseconds(uint16_t uSec);
-    void WriteMin(uint32_t duration) {}
 
 public:
     void Init(void);
-    void Arm(void);
+    MOTOR_STATE Arm(void);
+    bool isArmed(void) {return motorState == ARMED;}
+    
+    MOTOR_STATE SetSpeed(int16_t pct);
     void Stop(void) {WriteMicroseconds(oStop);}
-    void SetSpeed(int16_t pct);
-    void Calibrate(void);
 };
