5bae42c1c3c12e996302a6fbf631a56da20b273a
Greg <lewin.gc@gmail.com>
2024-03-02 18:53:43 -0500

    cleaned up defines

--- src/Motors-ROS.cpp
+++ src/Motors-ROS.cpp
@@ -17,11 +17,11 @@
 #define ENCODER_PIN1           8
 #define ENCODER_PIN2           9
 
-#define WHEEL_RADIUS            0.0210  // meters // 0.825/39.37; //inches to m
-#define WHEEL_CIRCUMFRANCE      0.0132  // meters // 2.0*PI*WHEEL_RADIUS;
-#define PPR                     1024.0  // pulses per revolution of the encoder
+//#define WHEEL_RADIUS            0.0210  // meters // 0.825/39.37; //inches to m
+//#define WHEEL_CIRCUMFRANCE      0.132   // meters // 2.0*PI*WHEEL_RADIUS;
+//#define PPR                     1024.0  // pulses per revolution of the encoder
 #define METERS_PER_TICK         0.0001286 // meters/tick
-#define CM_PER_TICK             0.01286 // cm/tick // maybe use this?
+//#define CM_PER_TICK             0.01286 // cm/tick // maybe use this?
 #define LOOP_RATE_MS            50
 
 QuadEncoder<ENCODER_PIN1,ENCODER_PIN2> encoder; 
@@ -88,6 +88,8 @@ void processEncoders(void)
 
     int32_t currTicks = encoder.TakeSnapshot();
     int32_t delta = encoder.CalcDelta();
+
+    DEBUG_SERIAL.println(currTicks);
 
     enc_val.data = currTicks;
     pub_enc.publish(&enc_val);
