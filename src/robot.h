af02e9cfe0cc9cf3ef766660b91c8c91ac2ec4af
Greg Lewin <gcl8a@icloud.com>
2024-06-09 17:48:31 -0400

    dropped vestigial comments

--- src/robot.h
+++ src/robot.h
@@ -8,8 +8,6 @@ public:
     enum ROBOT_STATE {ROBOT_IDLE, ROBOT_PATROLLING, ROBOT_APPROACHING, ROBOT_DETERRING};
     enum DIRECTION {DIR_HOLD, DIR_REV, DIR_FWD, DIR_MAX};
 
-    //int8_t readyToPID = 0;
-
 private:
     ROBOT_STATE robotState = ROBOT_IDLE;
     DIRECTION robotDirection = DIR_HOLD;
