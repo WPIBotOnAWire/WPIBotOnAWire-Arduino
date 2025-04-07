cfac2418b2b1ba5a50cfe9ca50f2b19687444eb0
Greg Lewin <gcl8a@icloud.com>
2025-04-06 20:57:53 -0400

    flash if camera sees closer than 100cm; untested

--- src/camera-ROS.cpp
+++ src/camera-ROS.cpp
@@ -1,13 +1,16 @@
 #include "camera-ROS.h"
-
+#include "led-ROS.h"
 #include "robot.h"
 #include <std_msgs/Int16.h>
+
+const float CAMERA_THRESHOLD = 100; // cm
 
 // callback function for receiving status commands
 void cbCameraDetection(const std_msgs::Int16& msg) 
 {
     int16_t distance = msg.data;
     robot.handleCameraReading(distance, Robot::DIR_FWD);
+    if(distance < CAMERA_THRESHOLD) setLED();
 }
 
 ros::Subscriber<std_msgs::Int16> camera_sub("/bird_distance_camera", cbCameraDetection);
